632250690172550081acc50d4e02a4914da62a67
Aastha Patel <56579556+aasthapatel97@users.noreply.github.com>
2021-08-13 17:15:15 +0530

    Update four.js

--- js/four.js
+++ js/four.js
@@ -6,10 +6,15 @@
 
 // Read database
 let fc = firebase.database().ref('deck/flashcards');
-{let jsonString;    
+let jsonString;
+fc.get().then((snapshot) => {
+    if (snapshot.exists()) {        
+        let queryData = snapshot.val();
+    
         displayQuestion(queryData, 3);
-        displayQuestion(queryData, 4);
-        displayQuestion(queryData, 5);
-        displayQuestion(queryData, 6);
-    };
-
+    } else {
+      console.log("No data available");
+    }
+}).catch((error) => {
+    console.error(error);
+});
