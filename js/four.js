1dda69a42b739ecc5eb4ee62f68deb2238944aa5
Aastha Patel <56579556+aasthapatel97@users.noreply.github.com>
2021-08-13 16:46:26 +0530

    Update four.js

--- js/four.js
+++ js/four.js
@@ -12,42 +12,8 @@ fc.get().then((snapshot) => {
         let queryData = snapshot.val();
     
         displayQuestion(queryData, 3);
-    } else {
-      console.log("No data available");
-    }
-}).catch((error) => {
-    console.error(error);
-});
-
-fc.get().then((snapshot) => {
-    if (snapshot.exists()) {        
-        let queryData = snapshot.val();
-    
         displayQuestion(queryData, 4);
-    } else {
-      console.log("No data available");
-    }
-}).catch((error) => {
-    console.error(error);
-});
-
-
-fc.get().then((snapshot) => {
-    if (snapshot.exists()) {        
-        let queryData = snapshot.val();
-    
         displayQuestion(queryData, 5);
-    } else {
-      console.log("No data available");
-    }
-}).catch((error) => {
-    console.error(error);
-});
-
-fc.get().then((snapshot) => {
-    if (snapshot.exists()) {        
-        let queryData = snapshot.val();
-    
         displayQuestion(queryData, 6);
     } else {
       console.log("No data available");
