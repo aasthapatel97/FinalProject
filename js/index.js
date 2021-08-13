ead2f352400b6c3ea8f6fb951e292c3783af5d5e
Aastha Patel <56579556+aasthapatel97@users.noreply.github.com>
2021-08-13 16:14:37 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,14 +1,14 @@
 let myFlashcards = [ 
     {
-        "question": "Name this species of bird.",
-        "hint": "h1",    
-        "skills": ["C#", 'Javascript'],
+        "question": "Q. How many total syllables are there in a Haiku?",
+        "hint": "If the syllable pattern is A-B-C, total syllables = A+B+C [Haiku's syllable pattern is 5-7-5]",    
+        "skills": ["Literary interpretation", 'Close reading of literature'],
         "image" : "images/barn-owl-2550068__340.webp",
         "type": 0,
         "answer": [
-            "Barn Owl",
-            "Dove",
-            "Hummingbird"
+            "25",
+            "17",
+            "10"
         ],
         "correct": 0,
         "response": {
@@ -17,15 +17,34 @@ let myFlashcards = [
         }
     },
     {
-        "question": "Name this species of river.",
-        "hint": "h2",    
-        "skills": ["NodeJS", 'Python'],
+        "question": "Q. Which line(s) have 7 syllables? ",
+        "hint": "Celebration (Ce-le-bra-tion) has 4 syllables",    
+        "skills": ["Syllable counting", 'Recognition of the conventions of traditional Japanese haiku'],
         "image" : "images/barn-owl-2550068__340.webp",
         "type": 0,
         "answer": [
-            "Nile",
-            "Amazon",
-            "Yangtze"
+            "My bunny is named June",
+            "What is your favorite color",
+            "I love giraffes and rabbits"
+        ],
+        "correct": 2,
+        "response": {
+            "Jack": 1,
+            "Bill": 0
+        }
+    },
+    {
+        "question": "Q. Complete the Haiku: 
+        An ocean voyage
+        As waves break over the bow",
+        "hint": "Count the number syllables",    
+        "skills": ["Creative Writing", 'Writing concretely and vividly'],
+        "image" : "images/barn-owl-2550068__340.webp",
+        "type": 0,
+        "answer": [
+            "the sea welcomes me.",
+            "I'm free to ride the waves",
+            "My boat suddenly, sinks."
         ],
         "correct": 0,
         "response": {
@@ -34,15 +53,15 @@ let myFlashcards = [
         }
     },
     {
-        "question": "Name this species of mountain.",
-        "hint": "h3",    
-        "skills": ["React", 'Python'],
+        "question": "Q. How many total syllables are there in a Haiku?",
+        "hint": "If the syllable pattern is A-B-C, total syllables = A+B+C [Haiku's syllable pattern is 5-7-5]",    
+        "skills": ["Literary interpretation", 'Close reading of literature'],
         "image" : "images/barn-owl-2550068__340.webp",
         "type": 0,
         "answer": [
-            "Lhotse",
-            "Makalu",
-            "Manaslu"
+            "25",
+            "17",
+            "10"
         ],
         "correct": 0,
         "response": {
@@ -51,13 +70,49 @@ let myFlashcards = [
         }
     },
     {
-        "question": "What is the longest river.",
-        "hint": "h4",    
-        "skills": ["React", 'Angular'],
+        "question": "Q. Which line(s) have 7 syllables? ",
+        "hint": "Celebration (Ce-le-bra-tion) has 4 syllables",    
+        "skills": ["Syllable counting", 'Recognition of the conventions of traditional Japanese haiku'],
+        "image" : "images/barn-owl-2550068__340.webp",
+        "type": 0,
+        "answer": [
+            "My bunny is named June",
+            "What is your favorite color",
+            "I love giraffes and rabbits"
+        ],
+        "correct": 2,
+        "response": {
+            "Jack": 1,
+            "Bill": 0
+        }
+    },
+    {
+        "question": "Q. Complete the Haiku: 
+        An ocean voyage
+        As waves break over the bow",
+        "hint": "Count the number syllables",    
+        "skills": ["Creative Writing", 'Writing concretely and vividly'],
+        "image" : "images/barn-owl-2550068__340.webp",
+        "type": 0,
+        "answer": [
+            "the sea welcomes me.",
+            "I'm free to ride the waves",
+            "My boat suddenly, sinks."
+        ],
+        "correct": 0,
+        "response": {
+            "Jack": 1,
+            "Bill": 0
+        }
+    },
+    {
+        "question": "Write a haiku",
+        "hint": "Unleash your creativity",    
+        "skills": ["Creative Writing", 'Writing concretely and vividly'],
         "image" : "images/barn-owl-2550068__340.webp",
         "type": 1,
-        "answer": "Nile",
-        "correct": "Nile",
+        "answer": "HAIKU",
+        "correct": "HAIKU",
         "response": {
             "Jack": "River1",
             "Bill": "River2"
